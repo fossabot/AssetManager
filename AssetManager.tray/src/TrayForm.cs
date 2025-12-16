2b8e07f9ecaca753dd223635b24ddf9643e812c2
Glauber x86 <glauber800br@gmail.com>
2025-12-16 16:17:47 -0300

    [Feat](API): Removido WebSocket e feito HTTP POST básico

--- AssetManager.tray/src/TrayForm.cs
+++ AssetManager.tray/src/TrayForm.cs
@@ -2,7 +2,9 @@ using System;
 using System.Windows.Forms;
 using System.ServiceProcess;
 using System.IO.Pipes;
-using System.Net.WebSockets;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 
 namespace AssetManager.Tray
 {
@@ -12,8 +14,10 @@ namespace AssetManager.Tray
         private readonly ContextMenuStrip trayMenu;
         private readonly HttpDebugService _httpDebugService = new();
         private const string ServiceName = "AssetManager";
-        private readonly MetricsWebSocketClient _wsClient;
-
+        private static readonly HttpClient _http = new();
+        private readonly string _apiUrl;
+        private readonly string _apiToken;
+        private readonly bool _apiEnabled;
         public TrayForm()
         {
             // Ocultar a janela
@@ -34,10 +38,18 @@ namespace AssetManager.Tray
                 Text = "Asset Manager Agent",
                 ContextMenuStrip = trayMenu
             };
-            var wsUrl = Program.Configuration["WebSocket:Url"];
-            var wsToken = Program.Configuration["WebSocket:Token"];
+            var config = Program.Configuration;
 
-            _wsClient = new MetricsWebSocketClient(wsUrl!, wsToken!);
+            _apiUrl = config["Api:Url"];
+            _apiToken = config["Api:Token"];
+
+            _apiEnabled = !string.IsNullOrWhiteSpace(_apiUrl);
+
+            if (_apiEnabled && !string.IsNullOrWhiteSpace(_apiToken))
+            {
+                _http.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _apiToken);
+            }
 
             _ = Task.Run(_httpDebugService.StartAsync);
             _ = Task.Run(PipeReadLoop);
@@ -119,16 +131,36 @@ namespace AssetManager.Tray
                     if (!string.IsNullOrWhiteSpace(json))
                     {
                         HttpDebugService.UpdateSnapshot(json);
-                        await _wsClient.SendAsync(json);
+                        _ = SendToApiAsync(json);
                     }
                 }
                 catch
                 {
-                    // serviço pode não estar pronto
-                    // TODO: adicionar logs
+                    // Serviço pode estar indisponível ainda
+                    // ignorar
                 }
-
                 await Task.Delay(2000);
+            }
+        }
+        private async Task SendToApiAsync(string json)
+        {
+            if (!_apiEnabled)
+                return;
+
+            try
+            {
+                using var content = new StringContent(
+                    json,
+                    Encoding.UTF8,
+                    "application/json"
+                );
+
+                await _http.PostAsync(_apiUrl!, content);
+            }
+            catch
+            {
+                // falha silenciosa
+                // tray nunca deve quebrar por rede
             }
         }
     }
